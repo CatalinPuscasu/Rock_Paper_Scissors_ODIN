1143684d65fc6af1e6653a6c51efa24c1e34b6e2
CatalinPuscasu <puscasucatalin437@gmail.com>
2021-12-25 23:17:53 +0200

    I have made the outputs for the PlayerChoice and also the Computer Choice both be visible on the page, with TextContent

--- app.js
+++ app.js
@@ -5,18 +5,20 @@
 
 
 
-//  game engine
+// //  game engine
+
 
 //  function computerPlay () {
 //     let words = ["Scissors", "Rock", "Paper"];
 //     const algorithm = words[Math.floor(Math.random()* words.length)];
 //     return(algorithm);
+    
 // }
 
 
 // let random = computerPlay();
 
-// // console.log(random);
+// console.log(random);
 
 // function Input () {
 //    let userInput = prompt("Please choose between Rock, Paper or Scissors");
@@ -32,43 +34,7 @@
 
 
 
-// function playRound (playerSelection, computerPlay) {
 
-    
-    
-//     if (playerInput === "Rock" && random === "Rock" ) {
-//         console.log("It's a tie");
-//     }
-//     else if (playerInput === "Rock" && random === "Paper") {
-//         console.log("You lost! Paper beats rock");
-//     }
-//     else if (playerInput === "Rock" && random === "Scissors") {
-//         console.log("You lost! Scissors beats rock");
-//     }
-//     else if (playerInput === "Paper" && random === "Scissors") {
-//         console.log("You lost! Scissors beats paper");
-//     }
-//     else if (playerInput === "Paper" && random === "Rock") {
-//         console.log("You won! Paper beats rock");
-//     }
-//     else if (playerInput === "Paper" && random === "Paper") {
-//         console.log("It's a tie!");
-//     }
-//     else if (playerInput === "Scissors" && random === "Paper") {
-//         console.log("You won! Scissors beats paper!");
-//     }
-//     else if (playerInput === "Scissors" && random === "Rock") {
-//         console.log("You lost! Rock beats Scissors");
-//     }
-//     else if (playerInput === "Scissors" && random === "Scissors") {
-//         console.log("It's a tie!");
-//     }
-
-// }
-
-// const playerSelection = playerInput;
-
-// const computerSelection = random;
 
 
 // console.log(playRound(playerSelection, computerSelection));
@@ -116,6 +82,8 @@
 // console.log(game(playerSelection, computerSelection));
 // console.log("To play another round, reload the page with the F5 key!");
 
+// trebuie sa ma gadnesc la cum sa apara si in consola rezultatul de la playerchoice, ca sa fac comparatia
+
 
 const btns = document.querySelectorAll('button');
 
@@ -124,12 +92,67 @@ let Paper = document.querySelector('.Paper');
 let Scissors = document.querySelector('.Scissors');
 
 
-btns.forEach(function (button) {
+const PlayerChoice = btns.forEach(function (button) {
     button.addEventListener('click', () => {
-        document.querySelector('p').textContent = 'You chose :' + "   " + button.textContent;
+     document.querySelector('p').textContent = 'You chose :' + "   " + button.textContent;
+     console.log(button.textContent);
+
     })
 });
 
+//  game engine
+
+
+const ComputerChoice =  function computerPlay () {
+    let words = ["Scissors", "Rock", "Paper"];
+    const algorithm = words[Math.floor(Math.random()* words.length)];
+    document.querySelector('.computer').textContent = 'The computer chose:' + "  " + algorithm;
+    return algorithm;
+    
+}
+
+
+
+ComputerChoice();
+
+
+function playRound (playerSelection, computerPlay) {
+
+    
+    
+    if (PlayerChoice === "Rock" && random === "Rock" ) {
+        console.log("It's a tie");
+    }
+    else if (PlayerChoice === "Rock" && random === "Paper") {
+        console.log("You lost! Paper beats rock");
+    }
+    else if (PlayerChoice === "Rock" && random === "Scissors") {
+        console.log("You lost! Scissors beats rock");
+    }
+    else if (PlayerChoice === "Paper" && random === "Scissors") {
+        console.log("You lost! Scissors beats paper");
+    }
+    else if (PlayerChoice === "Paper" && random === "Rock") {
+        console.log("You won! Paper beats rock");
+    }
+    else if (PlayerChoice === "Paper" && random === "Paper") {
+        console.log("It's a tie!");
+    }
+    else if (PlayerChoice === "Scissors" && random === "Paper") {
+        console.log("You won! Scissors beats paper!");
+    }
+    else if (PlayerChoice === "Scissors" && random === "Rock") {
+        console.log("You lost! Rock beats Scissors");
+    }
+    else if (PlayerChoice === "Scissors" && random === "Scissors") {
+        console.log("It's a tie!");
+    }
+
+}
+
+const playerSelection = PlayerChoice;
+
+// const computerSelection = random;
 
 
 
