372762c70f4df08aa610d1e938308a86a41c279f
CatalinPuscasu <puscasucatalin437@gmail.com>
2021-12-25 21:46:59 +0200

    Started to refactor, to rework on the logic of the game, to make it compatible with the buttons

--- app.js
+++ app.js
@@ -7,114 +7,114 @@
 
 //  game engine
 
- function computerPlay () {
-    let words = ["Scissors", "Rock", "Paper"];
-    const algorithm = words[Math.floor(Math.random()* words.length)];
-    return(algorithm);
-}
+//  function computerPlay () {
+//     let words = ["Scissors", "Rock", "Paper"];
+//     const algorithm = words[Math.floor(Math.random()* words.length)];
+//     return(algorithm);
+// }
 
 
-let random = computerPlay();
+// let random = computerPlay();
 
+// // console.log(random);
+
+// function Input () {
+//    let userInput = prompt("Please choose between Rock, Paper or Scissors");
+//      if (userInput !== null) {
+//          return userInput;
+//      }
+// }
+
+// let playerInput = Input();
+
+// console.log(playerInput);
 // console.log(random);
 
-function Input () {
-   let userInput = prompt("Please choose between Rock, Paper or Scissors");
-     if (userInput !== null) {
-         return userInput;
-     }
-}
-
-let playerInput = Input();
-
-console.log(playerInput);
-console.log(random);
 
 
-
-function playRound (playerSelection, computerPlay) {
+// function playRound (playerSelection, computerPlay) {
 
     
     
-    if (playerInput === "Rock" && random === "Rock" ) {
-        console.log("It's a tie");
-    }
-    else if (playerInput === "Rock" && random === "Paper") {
-        console.log("You lost! Paper beats rock");
-    }
-    else if (playerInput === "Rock" && random === "Scissors") {
-        console.log("You lost! Scissors beats rock");
-    }
-    else if (playerInput === "Paper" && random === "Scissors") {
-        console.log("You lost! Scissors beats paper");
-    }
-    else if (playerInput === "Paper" && random === "Rock") {
-        console.log("You won! Paper beats rock");
-    }
-    else if (playerInput === "Paper" && random === "Paper") {
-        console.log("It's a tie!");
-    }
-    else if (playerInput === "Scissors" && random === "Paper") {
-        console.log("You won! Scissors beats paper!");
-    }
-    else if (playerInput === "Scissors" && random === "Rock") {
-        console.log("You lost! Rock beats Scissors");
-    }
-    else if (playerInput === "Scissors" && random === "Scissors") {
-        console.log("It's a tie!");
-    }
+//     if (playerInput === "Rock" && random === "Rock" ) {
+//         console.log("It's a tie");
+//     }
+//     else if (playerInput === "Rock" && random === "Paper") {
+//         console.log("You lost! Paper beats rock");
+//     }
+//     else if (playerInput === "Rock" && random === "Scissors") {
+//         console.log("You lost! Scissors beats rock");
+//     }
+//     else if (playerInput === "Paper" && random === "Scissors") {
+//         console.log("You lost! Scissors beats paper");
+//     }
+//     else if (playerInput === "Paper" && random === "Rock") {
+//         console.log("You won! Paper beats rock");
+//     }
+//     else if (playerInput === "Paper" && random === "Paper") {
+//         console.log("It's a tie!");
+//     }
+//     else if (playerInput === "Scissors" && random === "Paper") {
+//         console.log("You won! Scissors beats paper!");
+//     }
+//     else if (playerInput === "Scissors" && random === "Rock") {
+//         console.log("You lost! Rock beats Scissors");
+//     }
+//     else if (playerInput === "Scissors" && random === "Scissors") {
+//         console.log("It's a tie!");
+//     }
 
-}
+// }
 
-const playerSelection = playerInput;
+// const playerSelection = playerInput;
 
-const computerSelection = random;
+// const computerSelection = random;
 
 
-console.log(playRound(playerSelection, computerSelection));
-//   Game function
+// console.log(playRound(playerSelection, computerSelection));
+// //   Game function
 
-function game(playerSelection, computerPlay) {
-    // this function will include all the game
+// function game(playerSelection, computerPlay) {
+//     // this function will include all the game
 
-    function playRound (playerSelection, computerPlay) {
+//     function playRound (playerSelection, computerPlay) {
 
     
     
-    if (playerInput === "Rock" && random === "Rock" ) {
-        console.log("It's a tie");
-    }
-    else if (playerInput === "Rock" && random === "Paper") {
-        console.log("You lost! Paper beats rock");
-    }
-    else if (playerInput === "Rock" && random === "Scissors") {
-        console.log("You lost! Scissors beats rock");
-    }
-    else if (playerInput === "Paper" && random === "Scissors") {
-        console.log("You lost! Scissors beats paper");
-    }
-    else if (playerInput === "Paper" && random === "Rock") {
-        console.log("You won! Paper beats rock");
-    }
-    else if (playerInput === "Paper" && random === "Paper") {
-        console.log("It's a tie!");
-    }
-    else if (playerInput === "Scissors" && random === "Paper") {
-        console.log("You won! Scissors beats paper!");
-    }
-    else if (playerInput === "Scissors" && random === "Rock") {
-        console.log("You lost! Rock beats Scissors");
-    }
-    else if (playerInput === "Scissors" && random === "Scissors") {
-        console.log("It's a tie!");
-    }
-    console.log(playRound(playerSelection, computerSelection));
-  }
+//     if (playerInput === "Rock" && random === "Rock" ) {
+//         console.log("It's a tie");
+//     }
+//     else if (playerInput === "Rock" && random === "Paper") {
+//         console.log("You lost! Paper beats rock");
+//     }
+//     else if (playerInput === "Rock" && random === "Scissors") {
+//         console.log("You lost! Scissors beats rock");
+//     }
+//     else if (playerInput === "Paper" && random === "Scissors") {
+//         console.log("You lost! Scissors beats paper");
+//     }
+//     else if (playerInput === "Paper" && random === "Rock") {
+//         console.log("You won! Paper beats rock");
+//     }
+//     else if (playerInput === "Paper" && random === "Paper") {
+//         console.log("It's a tie!");
+//     }
+//     else if (playerInput === "Scissors" && random === "Paper") {
+//         console.log("You won! Scissors beats paper!");
+//     }
+//     else if (playerInput === "Scissors" && random === "Rock") {
+//         console.log("You lost! Rock beats Scissors");
+//     }
+//     else if (playerInput === "Scissors" && random === "Scissors") {
+//         console.log("It's a tie!");
+//     }
+//     console.log(playRound(playerSelection, computerSelection));
+//   }
 
-}
+// }
 
-console.log(game(playerSelection, computerSelection));
-console.log("To play another round, reload the page with the F5 key!");
+// console.log(game(playerSelection, computerSelection));
+// console.log("To play another round, reload the page with the F5 key!");
 
 
 
